d2f9353898d755337702465d9e6671d927500856
Philip Bui <philip.bui.developer@gmail.com>
2019-01-14 12:00:00 +1100

    Completed S2CellId Tests
    Added getRandomCellId in GeometryTestCase.
    
    Added testInverses, testToToken, testTokens, testContainment, testContinuity, testCoverage, testAllNeighbours, testNeighbours to S2CellIdTests.

--- Tests/GeometryTestCase.swift
+++ Tests/GeometryTestCase.swift
@@ -11,4 +11,14 @@ import XCTest
 
 class GeometryTestCase: XCTestCase {
 
+    /**
+     * Return a random cell id at the given level or at a randomly chosen level.
+     * The distribution is uniform over the space of cell ids, but only
+     * approximately uniform over the surface of the sphere.
+     */
+    public func getRandomCellId(level: Int = Int.random(in: 0..<S2CellId.maxLevel)) -> S2CellId {
+        let face = Int.random(in: 0..<S2CellId.numFaces)
+        let pos = Int64.random(in: 0...Int64.max) & ((Int64(1) << (2 * S2CellId.maxLevel)) - Int64(1))
+        return S2CellId(face: face, pos: pos, level: level)
+    }
 }
